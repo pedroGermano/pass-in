2424d4c8ceff5d524881c83815f42730c0c02de7
Pedro Germano <pedrogermano232@gmail.com>
2024-04-02 04:46:29 -0300

    feat: first day done

--- web/src/components/attendeeList.tsx
+++ web/src/components/attendeeList.tsx
@@ -1,14 +1,21 @@
-import { Search } from "lucide-react";
+import {
+  Search,
+  MoreHorizontal,
+  ChevronsLeft,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsRight,
+} from "lucide-react";
 
 export function AttendeeList() {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center gap-3">
         <h1 className="text-2xl font-bold">Participantes</h1>
-        <div className="px-3 py-1.5 flex items-center gap-3 border w-72 border-white/10  rounded-lg text-sm">
+        <div className="px-3 py-1.5 flex items-center gap-3 border w-72 border-white/10  rounded-lg ">
           <Search className="size-4 text-emerald-300" />
           <input
-            className="flex-1 bg-transparent outline-none"
+            className="flex-1 p-0 text-sm bg-transparent border-0 outline-none"
             type="text"
             placeholder="Buscar participante..."
           />
@@ -18,8 +25,14 @@ export function AttendeeList() {
         <table className="w-full">
           <thead>
             <tr className="border-b border-white/10">
-              <th className="px-4 py-3 text-sm font-semibold text-left">
-                <input type="checkbox" />
+              <th
+                style={{ width: 48 }}
+                className="px-4 py-3 text-sm font-semibold text-left"
+              >
+                <input
+                  className="rounded size-4 bg-black/20 border-white/10"
+                  type="checkbox"
+                />
               </th>
               <th className="px-4 py-3 text-sm font-semibold text-left">
                 Código
@@ -33,15 +46,24 @@ export function AttendeeList() {
               <th className="px-4 py-3 text-sm font-semibold text-left">
                 Data do check-in
               </th>
-              <th className="px-4 py-3 text-sm font-semibold text-left"></th>
+              <th
+                style={{ width: 64 }}
+                className="px-4 py-3 text-sm font-semibold text-left"
+              ></th>
             </tr>
           </thead>
           <tbody>
-            {Array.from({ length: 10 }).map(() => {
+            {Array.from({ length: 8 }).map((_, i) => {
               return (
-                <tr className="border-b border-white/10">
+                <tr
+                  key={i}
+                  className="border-b border-white/10 hover:bg-white/5"
+                >
                   <td className="px-4 py-3 text-sm text-zinc-300">
-                    <input type="checkbox" />
+                    <input
+                      className="rounded size-4 bg-black/20 border-white/10"
+                      type="checkbox"
+                    />
                   </td>
                   <td className="px-4 py-3 text-sm text-zinc-300">12234</td>
                   <td className="px-4 py-3 text-sm text-zinc-300">
@@ -54,7 +76,11 @@ export function AttendeeList() {
                   </td>
                   <td className="px-4 py-3 text-sm">7 dias atrás</td>
                   <td className="px-4 py-3 text-sm">3 dias atrás</td>
-                  <td className="px-4 py-3 text-sm"></td>
+                  <td className="px-4 py-3 text-sm">
+                    <button className="rounded-md bg-black/20 border-white/10 p-1.5">
+                      <MoreHorizontal className="size-4" />
+                    </button>
+                  </td>
                 </tr>
               );
             })}
@@ -65,7 +91,23 @@ export function AttendeeList() {
                 Monstrando 10 de 228 itens
               </td>
               <td className="px-4 py-3 text-sm text-right" colSpan={3}>
-                Página 1 de 23
+                <div className="inline-flex items-center gap-8 ">
+                  <span>Página 1 de 23</span>
+                  <div className="flex gap-1.5">
+                    <button className="rounded-md bg-white/10 border-white/10 p-1.5">
+                      <ChevronsLeft className="size-4" />
+                    </button>
+                    <button className="rounded-md bg-white/10 border-white/10 p-1.5">
+                      <ChevronLeft className="size-4" />
+                    </button>
+                    <button className="rounded-md bg-white/10 border-white/10 p-1.5">
+                      <ChevronRight className="size-4" />
+                    </button>
+                    <button className="rounded-md bg-white/10 border-white/10 p-1.5">
+                      <ChevronsRight className="size-4" />
+                    </button>
+                  </div>
+                </div>
               </td>
             </tr>
           </tfoot>
