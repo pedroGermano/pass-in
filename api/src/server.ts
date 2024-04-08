16607edad52ca877b65397d7225bb36064e482db
Pedro Germano <pedrogermano232@gmail.com>
2024-04-07 21:20:24 -0300

    feat: refactor register for events

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,12 +4,14 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-events";
+import { registerForEvent } from "./routes/register-for-events";
 
 const app = fastify();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(registerForEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
