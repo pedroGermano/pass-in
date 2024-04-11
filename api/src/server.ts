9f6001897f913ccf7b01c15488d43ccfb9a2be63
Pedro Germano <pedrogermano232@gmail.com>
2024-04-11 12:22:41 -0300

    feat: route

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,6 +6,7 @@ import {
 import { createEvent } from "./routes/create-events";
 import { registerForEvent } from "./routes/register-for-events";
 import { getEvent } from "./routes/get-events";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify();
 app.setValidatorCompiler(validatorCompiler);
@@ -14,6 +15,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
+app.register(getAttendeeBadge);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
